7a0d1e880314adbbefebe9a927f6ee8f1ba8e0cc
cometj03 <harryjung1001@gmail.com>
2023-10-07 21:31:14 +0900

    2023.10.07

--- PS/28325.cpp
+++ PS/28325.cpp
@@ -1,26 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
-ll C[250505], N;
-
-// DP[i][0] : 1~i까지 중에서 i번째를 선택하지 않았을 때의 최대
-// DP[i][1] : i번째를 선택했을 때의 최대
-ll DP[250505][2];
+ll C[250505], N, sum = 0;
+vector<int> idx;
 
 int main() {
 	cin.tie(0), ios_base::sync_with_stdio(0);
 	cin >> N;
-	for (int i = 1; i <= N; i++) cin >> C[i];
 	for (int i = 1; i <= N; i++) {
-		if (i == N) {
-			// 1번째에 쪽방이 있으면 무조건 쪽방을 선택한다고 가정
-			// => C[1] != 0이면 DP[1][0]으로 해도 됨
-			if (C[1] != 0) DP[i][1] = DP[i - 1][0] + 1;
-			DP[i][0] = max(DP[i - 1][0], DP[i - 1][1]) + C[i];
-			continue;
-		}
-		DP[i][0] = max(DP[i - 1][0], DP[i - 1][1]) + C[i];	// 쪽방이 없으면 C[i] == 0이라 상관 없음
-		DP[i][1] = DP[i - 1][0] + 1;
+		cin >> C[i];
+		sum += C[i];
+		if (C[i] != 0) idx.push_back(i);
 	}
-	cout << max(DP[N][0], DP[N][1]);
+	if (idx.size() <= 1)
+		cout << sum + N / 2;
+	else {
+		for (int i = 0; i < idx.size() - 1; i++) {
+			sum += (idx[i + 1] - idx[i]) / 2;
+		}
+		sum += (N - idx.back() + idx[0]) / 2;
+		cout << sum;
+	}
 }
