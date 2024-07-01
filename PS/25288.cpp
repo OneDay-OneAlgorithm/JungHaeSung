a37959e0f6eec8ac573b7f0083bb62ba4b53cc9e
cometj03 <harryjung1001@gmail.com>
2024-07-01 19:12:03 +0900

    clean

--- PS/25288.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int main() {
-	int N;
-	string s;
-	cin >> N >> s;
-	while (N--) cout << s;
-}
--- PS/kotlin/src/main/kotlin/10953.kt
+++
@@ -1,8 +0,0 @@
-fun main() {
-    val t = readln().toInt()
-    buildString {
-        for (i in 1..t) {
-            appendLine(readln().split(",").map(String::toInt).sum())
-        }
-    }.also(::print)
-}
